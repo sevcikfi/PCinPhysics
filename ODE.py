891cecd64acf72d3fa4c584c2ef9a28fb87f9391
Hayaka <fisevcik@gmail.com>
2022-03-24 01:23:16 +0100

    name == main for ODE.py

--- ODE.py
+++ ODE.py
@@ -60,3 +60,27 @@ def ode_solve(model, initial_conditions, integrator=runge_kutta_4, dt=0.1, maxt=
         ti = ti1
 
     return np.array(y), np.array(t)
+
+if __name__ == "__main__":  
+    y0 = 0
+    v0 = 1
+    dt = 0.1
+    maxt = 100
+
+    def model(y, t):
+        x, v = y
+        return np.array([v, -x])
+
+
+    y1, t = ode_solve(model, [y0, v0],euler_2, dt, maxt)
+    y2, t  = ode_solve(model, [y0, v0], dt=dt, maxt=maxt)
+    y = np.sin(t)
+
+    plt.plot(t, y2[:, 0], label = f"sinus Euler1 with step = {dt}")
+    plt.plot(t, y1[:, 0], label = f"sinus Euler2 with step = {dt}") 
+    plt.plot(t, y, label = f"Vanila sinus") 
+
+    plt.xlabel("time t")
+    plt.ylabel("distance y")
+    plt.legend()
+    plt.show()
