fc445352bcc34f359f4ae10b6cee20b405a266cc
Hayaka <fisevcik@gmail.com>
2022-03-24 01:23:28 +0100

    Homework 2 done

--- functions.py
+++ functions.py
@@ -16,15 +16,38 @@ def g(X, parameters=(1, 100)):
     a, b = parameters
     return (a - x)**2 + b * (y - x * x)**2
 
-fnc = g
-path = minimize.minimize(fnc, initial_condition=(2 * random.random() - 1, 2 * random.random() - 1))
+def h(X):
+    """ h function"""
+    s, t, u, v = X
+    sum2 = s**2 + t**2 + u**2 + v**2
+    if sum2 > 2:
+        return float("inf")
 
-print("Position of the minimum:", path[-1])
-print(f"Minimum value of function {fnc.__name__}:", fnc(path[-1]))
+    k2 = (s**2 + t**2) * (2 - sum2) + (s * u - t * v)**2
+    k3 = np.sqrt(2 - sum2)
+    return (sum2 / 2) - (k2 / 4) + (k3 * s / 4)
 
-# Several trajectories from random position into the minimum
-for i in range(10):
+def Rosenbrock_function(X: np.ndarray, params=(1, 100)):
+    """Rosenbrock function"""
+    D = len(X) - 1  #works only if 1d array
+    a, b = params
+    sum = 0
+    for i in range(D - 1):
+        sum += (a - X[i]**2 + b*(X[i+1] - X[i]**2 )**2 )
+
+    return sum
+
+
+if __name__ == "__main__":
+    fnc = g
     path = minimize.minimize(fnc, initial_condition=(2 * random.random() - 1, 2 * random.random() - 1))
-    plt.plot(path[:,0], path[:,1])
-
-plt.show()
+    
+    print("Position of the minimum:", path[-1])
+    print(f"Minimum value of function {fnc.__name__}:", fnc(path[-1]))
+    
+    # Several trajectories from random position into the minimum
+    for i in range(10):
+        path = minimize.minimize(fnc, initial_condition=(2 * random.random() - 1, 2 * random.random() - 1))
+        plt.plot(path[:,0], path[:,1])
+    
+    plt.show()
--- program.py
+++ program.py
@@ -1,26 +1,27 @@
-import ODE
 import numpy as np
 import matplotlib.pyplot as plt
 
-y0 = 0
-v0 = 1
-dt = 0.1
-maxt = 100
+import minimize
+import functions
 
-def model(y, t):
-    x, v = y
-    return np.array([v, -x])
+# (b - a) * random() + a from interval [a, b)
 
+fnc = functions.h
+path = minimize.minimize_adaptive(fnc, dimension=4, initial_condition=(0.1 * np.random.rand(4)))
 
-y1, t = ODE.ode_solve(model, [y0, v0],ODE.euler_2, dt, maxt)
-y2, t  = ODE.ode_solve(model, [y0, v0], dt=dt, maxt=maxt)
-y = np.sin(t)
+print("Position of the minimum:", path[-1])
+print(f"Minimum value of function {fnc.__name__}:", fnc(path[-1]))
 
-plt.plot(t, y2[:, 0], label = f"sinus Euler1 with step = {dt}")
-plt.plot(t, y1[:, 0], label = f"sinus Euler2 with step = {dt}") 
-plt.plot(t, y, label = f"Vanila sinus") 
+#Gets stuck at generating D=7 sphere    
+#fnc = functions.Rosenbrock_function
+#path = minimize.minimize_adaptive(fnc, dimension=7, initial_condition=(10 * np.random.rand(7) - 5))
 
-plt.xlabel("time t")
-plt.ylabel("distance y")
-plt.legend()
-plt.show()
+print("Position of the minimum:", path[-1])
+print(f"Minimum value of function {fnc.__name__}:", fnc(path[-1]))
+
+# Several trajectories from random position into the minimum
+for i in range(10):
+    path = minimize.minimize_adaptive(fnc, dimension=4, initial_condition=(0.1 * np.random.rand(4)))
+#    plt.plot(path[:,0], path[:,1])
+#    
+#plt.show()
