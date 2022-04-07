704071260eec985b2b22266b197b4cace7bf1437
Hayaka <fisevcik@gmail.com>
2022-04-07 16:49:17 +0200

    gauss not working

--- gauss.py
+++ gauss.py
@@ -1,5 +1,7 @@
-from unittest import result
 import numpy as np
+import matplotlib.pyplot as plt
+import histogram
+
 
 def gaussian_distribution(x):
     return 1 / np.sqrt(2 * np.pi) * np.exp(-x**2 / 2)
@@ -11,3 +13,25 @@ def generator_rand(N=1):
         if y < gaussian_distribution(x):
             return x
 
+def generator_clt():
+    """central limit theorem"""
+    return sum(np.random.random(12)) - 6
+    
+
+if __name__ == "__main__":
+    N = 100000
+    m = 12
+    data = np.zeros(N)
+    for _ in range(m):
+        data += np.random.random(N)
+    
+    data_clt = [generator_clt() for _ in range(N)]
+    data_rnd = [generator_rand() for _ in range(N)]
+
+    bins, vals = histogram.basicHistogram(data, normalize=True)
+    plt.plot(bins-(m/2), vals)
+    #plt.plot(bins-(m/2), vals)
+    #Gaus
+    #plt.plot()
+    plt.ylim(0)
+    plt.show()
